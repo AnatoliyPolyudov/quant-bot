a47b87fef2dc71427b3b12e45c5a5f2778ab0108
AnatoliyPolyudov <49663495+AnatoliyPolyudov@users.noreply.github.com>
2025-11-17 15:34:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ SYMBOL = "BTC-USDT-SWAP"
 BUCKET_SECONDS = 60.0
 
 # Единственный параметр стратегии
-IMBALANCE_THRESHOLD = 0.65  # 65% имбаланс
+IMBALANCE_THRESHOLD = 0.7
 
 # Параметры анти-спуфинга
-MIN_VOLUME_ANTI_SPOOFING = 8.0  # Минимальный объем для доверия имбалансу
+MIN_VOLUME_ANTI_SPOOFING = 0.5  # Минимальный объем для доверия имбалансу
